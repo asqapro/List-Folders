e690c2f77e7a218ff5baeb1c322e46a30a678555
Jeff Goldman <palidin112@hotmail.com>
2015-04-29 06:13:53 -0500

    Create Headers.h
    Combines all of the necessary headers into one simple file.

---
+++ Headers.h
@@ -0,0 +1,12 @@
+#ifndef HEADERS_H_INCLUDED
+#define HEADERS_H_INCLUDED
+
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include "DisplayClassHeader.h"
+#include "GlobalDirClassHeader.h"
+
+#endif // HEADERS_H_INCLUDED
